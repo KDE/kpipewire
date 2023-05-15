0669bb267ad9cfe04cd400624dd32ba6c4f5bcc1
David Edmundson <kde@davidedmundson.co.uk>
2023-05-15 08:58:43 +0300

    Drop unused method
    This code moved into VAAPI Utils

--- src/pipewireproduce.cpp
+++ src/pipewireproduce.cpp
@@ -158,39 +158,6 @@ void PipeWireProduce::setMaxFramerate(const Fraction &framerate)
     m_stream->setMaxFramerate(framerate);
 }
 
-static int setHwframeCtx(AVCodecContext *ctx, AVBufferRef *hw_device_ctx, const QSize &size)
-{
-    AVBufferRef *hw_frames_ref;
-    AVHWFramesContext *frames_ctx = NULL;
-    int err = 0;
-
-    if (!(hw_frames_ref = av_hwframe_ctx_alloc(hw_device_ctx))) {
-        fprintf(stderr, "Failed to create VAAPI frame context.\n");
-        return -1;
-    }
-    frames_ctx = (AVHWFramesContext *)(hw_frames_ref->data);
-    frames_ctx->format = AV_PIX_FMT_VAAPI;
-    frames_ctx->sw_format = AV_PIX_FMT_NV12;
-    frames_ctx->width = size.width();
-    frames_ctx->height = size.height();
-    frames_ctx->initial_pool_size = 20;
-    if ((err = av_hwframe_ctx_init(hw_frames_ref)) < 0) {
-        fprintf(stderr,
-                "Failed to initialize VAAPI frame context."
-                "Error code: %s\n",
-                av_err2str(err));
-        av_buffer_unref(&hw_frames_ref);
-        return err;
-    }
-    ctx->hw_frames_ctx = av_buffer_ref(hw_frames_ref);
-    if (!ctx->hw_frames_ctx) {
-        err = AVERROR(ENOMEM);
-    }
-
-    av_buffer_unref(&hw_frames_ref);
-    return err;
-}
-
 void PipeWireProduce::initFiltersVaapi()
 {
     m_avFilterGraph = avfilter_graph_alloc();
