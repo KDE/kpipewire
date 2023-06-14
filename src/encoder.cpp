08073b4dcf2e946ed187197cb0c2ecc69d4c3e2a
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-06-14 15:16:26 +0200

    doc

--- src/encoder.cpp
+++ src/encoder.cpp
@@ -1,5 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2023 Aleix Pol Gonzalez <aleixpol@kde.org>
+    SPDX-FileCopyrightText: 2023 Marco Martin <mart@kde.org>
     SPDX-FileCopyrightText: 2023 Arjen Hiemstra <ahiemstra@heimr.nl>
 
     SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
