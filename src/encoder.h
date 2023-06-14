f0a22da766c5602406f2727418bee871e8619a5d
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-06-14 15:10:22 +0200

    doc

--- src/encoder.h
+++ src/encoder.h
@@ -21,18 +21,53 @@ char *av_err2str(int errnum);
 struct PipeWireFrame;
 class PipeWireProduce;
 
+/**
+ * Base class for objects that encapsulate encoder logic and state.
+ */
 class Encoder : public QObject
 {
     Q_OBJECT
 public:
+    /**
+     * Constructor.
+     *
+     * @param produce The PipeWireProduce instance that owns this encoder.
+     */
     Encoder(PipeWireProduce *produce);
     ~Encoder() override;
 
+    /**
+     * Initialize and setup the encoder.
+     *
+     * @param size The size of the stream being encoded.
+     *
+     * @return true if initailization was succesful, false if not.
+     */
     virtual bool initialize(const QSize &size) = 0;
+    /**
+     * Process a PipeWire frame and pass it to libav for filtering.
+     *
+     * @param frame The frame to process.
+     *
+     * @note This method will be called on its own thread.
+     */
     virtual void filterFrame(const PipeWireFrame &frame) = 0;
+    /**
+     * Get the next finished frames from the libav filter chain and queue them for encoding.
+     *
+     * @note This method will be called on its own thread.
+     */
     virtual void encodeFrame();
+    /**
+     * Get the next encoded frames from libav and pass them to PipeWireProduce.
+     *
+     * @note This method will be called on its own thread.
+     */
     virtual void receivePacket();
 
+    /**
+     * Return the AVCodecContext for this encoder.
+     */
     AVCodecContext *avCodecContext() const;
 
 protected:
@@ -46,6 +81,9 @@ protected:
     AVFilterContext *m_outputFilter = nullptr;
 };
 
+/**
+ * Encoder subclass that can be used as base class for software encoders.
+ */
 class SoftwareEncoder : public Encoder
 {
 public:
@@ -54,6 +92,9 @@ public:
     void filterFrame(const PipeWireFrame &frame) override;
 };
 
+/**
+ * Encoder subclass that can be used as base class for hardware encoders.
+ */
 class HardwareEncoder : public Encoder
 {
 public:
@@ -63,7 +104,26 @@ public:
     void filterFrame(const PipeWireFrame &frame) override;
 
 protected:
+    /**
+     * Check if VAAPI is supported for a given size stream.
+     *
+     * @param size The size of the stream to check.
+     *
+     * @return The path to a device node that can encode this stream. If
+     *         the stream cannot be encoded by the current hardware, an
+     *         empty QByteArray will be returned.
+     */
     QByteArray checkVaapi(const QSize &size);
+    /**
+     * Create the libav contexts for the DRM subsystem.
+     *
+     * These contexts are used when doing import of dma-buf based frames.
+     *
+     * @param path The path to a device node where the frames are.
+     * @param size The size of the frames.
+     *
+     * @return true if the contexts were successfully created, false if not.
+     */
     bool createDrmContext(const QByteArray &path, const QSize &size);
 
     AVBufferRef *m_drmContext;
