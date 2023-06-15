e23bf462a174b731c7daabf367d8bdaefc6fa6a1
Arjen Hiemstra <ahiemstra@heimr.nl>
2023-06-15 13:55:43 +0200

    Add LibVpxEncoder as software encoder for VP8

---
+++ src/libvpxencoder.h
@@ -0,0 +1,20 @@
+/*
+    SPDX-FileCopyrightText: 2023 Aleix Pol Gonzalez <aleixpol@kde.org>
+    SPDX-FileCopyrightText: 2023 Marco Martin <mart@kde.org>
+    SPDX-FileCopyrightText: 2023 Arjen Hiemstra <ahiemstra@heimr.nl>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#include "encoder.h"
+
+/**
+ * A software encoder that uses libvpx to encode to VP8.
+ */
+class LibVpxEncoder : public SoftwareEncoder
+{
+public:
+    LibVpxEncoder(PipeWireProduce *produce);
+
+    bool initialize(const QSize &size) override;
+};
