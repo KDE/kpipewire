7dc2c8e743eb8fb79316a7d0ba77f06323618c70
Luigi Toscano <luigi.toscano@tiscali.it>
2023-07-17 15:37:19 +0200

    Fix the translation template version number (5->6)
    Even thought this is part of Plasma which can't be co-installed with its
    previous version, it is aligned with the version of the component.

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,4 +3,4 @@
 # Extract strings from all source files.
 # If your framework depends on KI18n, use $XGETTEXT. If it uses Qt translation
 # system, use $EXTRACT_TR_STRINGS.
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kpipewire5.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kpipewire6.pot
